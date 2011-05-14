3fdafe75fdcc7737e641090adb3a4717982d0489
orlinmp@gmail.com <orlinmp@gmail.com@484e4852-73e6-1534-6742-d03849e20051>
2011-05-14 15:27:57 +0000

    test

--- trunk/ControlTestApp/AutoSuggestTextBox/CityEditWindow.xaml.cs
+++ trunk/ControlTestApp/AutoSuggestTextBox/CityEditWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows.Shapes; 
 
 namespace ControlTestApp.AutoSuggestTextBox
 {
